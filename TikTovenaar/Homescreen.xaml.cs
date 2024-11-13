bc648c3c5fb78d8b49ecf4f8f400fcdadd888ca2
GerbenVDH <144111280+GerbenVDH@users.noreply.github.com>
2024-11-13 12:20:15 +0100

    FIX: screens swithcing using user control

---
+++ TikTovenaar/Homescreen.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace TikTovenaar
+{
+    public partial class Homescreen : UserControl
+    {
+        public Homescreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
