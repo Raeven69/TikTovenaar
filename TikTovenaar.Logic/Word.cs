2a6e7b11b085d2f2ce3c62d4bb0cf5b2d4230937
KaseToatz1337 <112391293+Raeven69@users.noreply.github.com>
2024-11-13 13:59:08 +0100

    Add basic text functionality

---
+++ TikTovenaar.Logic/Word.cs
@@ -0,0 +1,41 @@
+﻿namespace TikTovenaar.Logic
+{
+    public class Word
+    {
+        public List<Letter> Letters { get; } = [];
+        public bool IsCompleted { get; private set; } = false;
+        private int Index { get; set; } = 0;
+
+        public Word(string word)
+        { 
+            foreach (char c in word)
+            {
+                Letters.Add(new(c));
+            }
+        }
+
+        public void EnterChar(char c)
+        {
+            if (c == ' ')
+            {
+                IsCompleted = true;
+            }
+            else if (!IsCompleted)
+            {
+                if (Index > Letters.Count - 1)
+                {
+                    Letter newLetter = new(null)
+                    {
+                        Received = c
+                    };
+                    Letters.Add(newLetter);
+                }
+                else
+                {
+                    Letters[Index].Received = c;
+                }
+                Index++;
+            }
+        }
+    }
+}
