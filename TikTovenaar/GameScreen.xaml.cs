d8652a9c17e71327f75cc8dcc9d8f006ddf45fa4
KaseToatz1337 <112391293+Raeven69@users.noreply.github.com>
2024-11-14 09:42:51 +0100

    Move logic to gamescreen

---
+++ TikTovenaar/GameScreen.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TikTovenaar.Logic;
+
+namespace TikTovenaar
+{
+    /// <summary>
+    /// Interaction logic for GameScreen.xaml
+    /// </summary>
+    public partial class GameScreen : UserControl
+    {
+        private Game Game { get; set; }
+
+        public GameScreen()
+        {
+            InitializeComponent();
+            Game = new();
+            UpdateWord();
+        }
+
+        public void OnKeyPress(object sender, KeyEventArgs args)
+        {
+            string key = args.Key.ToString();
+            if (key.Equals("Space"))
+            {
+                Game.PressKey(' ');
+            }
+            else if (key.Length == 1)
+            {
+                if (!Keyboard.Modifiers.HasFlag(ModifierKeys.Shift) && !Console.CapsLock)
+                {
+                    key = key.ToLower();
+                }
+                Game.PressKey(key[0]);
+            }
+            if (!Game.Finished)
+            {
+                UpdateWord();
+            }
+        }
+
+        public void UpdateWord()
+        {
+            if (Game.CurrentWord != null)
+            {
+                Text.Inlines.Clear();
+                foreach (Letter letter in Game.CurrentWord.Letters)
+                {
+                    Run run = new(letter.Received != null ? letter.Received.ToString() : letter.Value.ToString());
+                    if (letter.IsCorrect)
+                    {
+                        run.Foreground = new SolidColorBrush(Colors.Green);
+                    }
+                    else if (letter.HasGuessed)
+                    {
+                        run.Foreground = new SolidColorBrush(Colors.Red);
+                    }
+                    Text.Inlines.Add(run);
+                }
+            }
+        }
+    }
+}
--- TikTovenaar/MainWindow.xaml.cs
+++ TikTovenaar/MainWindow.xaml.cs
@@ -1,63 +1,12 @@
 ﻿using System.Windows;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using TikTovenaar.Logic;
 
 namespace TikTovenaar
 {
     public partial class MainWindow : Window
     {
-        private Game Game { get; set; }
-
         public MainWindow()
         {
             InitializeComponent();
-            Game = new();
-            UpdateWord();
-            MainContentControl.Content = new Loginscreen();
-        }
-
-        public void OnKeyPress(object sender, KeyEventArgs args)
-        {
-            string key = args.Key.ToString();
-            if (key.Equals("Space"))
-            {
-                Game.PressKey(' ');
-            }
-            else if (key.Length == 1)
-            {
-                if (!Keyboard.Modifiers.HasFlag(ModifierKeys.Shift) && !Console.CapsLock)
-                {
-                    key = key.ToLower();
-                }
-                Game.PressKey(key[0]);
-            }
-            if (!Game.Finished)
-            {
-                UpdateWord();
-            }
-        }
-
-        public void UpdateWord()
-        {
-            if (Game.CurrentWord != null)
-            {
-                Text.Inlines.Clear();
-                foreach (Letter letter in Game.CurrentWord.Letters)
-                {
-                    Run run = new(letter.Received != null ? letter.Received.ToString() : letter.Value.ToString());
-                    if (letter.IsCorrect)
-                    {
-                        run.Foreground = new SolidColorBrush(Colors.Green);
-                    }
-                    else if (letter.HasGuessed)
-                    {
-                        run.Foreground = new SolidColorBrush(Colors.Red);
-                    }
-                    Text.Inlines.Add(run);
-                }
-            }
         }
 
         public void SwitchToHomeScreen()
