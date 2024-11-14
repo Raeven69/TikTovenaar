adb866cf1d58f0b5178fc8eec9aca04482711d75
KaseToatz1337 <112391293+Raeven69@users.noreply.github.com>
2024-11-14 13:26:24 +0100

    First few tests

---
+++ TikTovenaar.Test/WordTest.cs
@@ -0,0 +1,106 @@
+﻿using TikTovenaar.Logic;
+
+namespace TikTovenaar.Test
+{
+    [TestClass]
+    public class WordTest
+    {
+        [TestMethod]
+        public void Constructor_LettersCountEquals()
+        {
+            string value = "test";
+            Word word = new(value);
+            Assert.AreEqual(value.Length, word.Letters.Count);
+        }
+
+        [TestMethod]
+        public void Constructor_FirstLetterEquals()
+        {
+            string value = "test";
+            Word word = new(value);
+            Assert.AreEqual(value.First(), word.Letters.Last().Value);
+        }
+
+        [TestMethod]
+        public void Constructor_LastLetterEquals()
+        {
+            string value = "test";
+            Word word = new(value);
+            Assert.AreEqual(value.Last(), word.Letters.Last().Value);
+        }
+
+        [TestMethod]
+        public void EnterChar_AllKeysPressed_NotCompleted()
+        {
+            Word word = new("test");
+            foreach (Letter letter in word.Letters)
+            {
+                word.EnterChar(letter.Value ?? ' ');
+            }
+            Assert.IsFalse(word.IsCompleted);
+        }
+
+        [TestMethod]
+        public void EnterChar_SpacePressed_Completed()
+        {
+            Word word = new("test");
+            word.EnterChar(' ');
+            Assert.IsTrue(word.IsCompleted);
+        }
+
+        [TestMethod]
+        public void EnterChar_FirstReceived_ShouldEqual()
+        {
+            string value = "test";
+            Word word = new(value);
+            foreach (Letter letter in word.Letters)
+            {
+                word.EnterChar(letter.Value ?? ' ');
+            }
+            Assert.AreEqual(value.First(), word.Letters.First().Received);
+        }
+
+        [TestMethod]
+        public void EnterChar_FirstReceived_ShouldNotEqual()
+        {
+            string value = "teste";
+            string rev = new(value.Reverse().ToArray());
+            Word word = new(value);
+            for (int i = 0; i < rev.Length; i++)
+            {
+                word.EnterChar(rev[i]);
+            }
+            Assert.AreNotEqual(value[0], word.Letters.First().Received);
+        }
+
+        [TestMethod]
+        public void EnterChar_LastReceived_ShouldEqual()
+        {
+            string value = "test";
+            Word word = new(value);
+            foreach (Letter letter in word.Letters)
+            {
+                word.EnterChar(letter.Value ?? ' ');
+            }
+            Assert.AreEqual(value.Last(), word.Letters.Last().Received);
+        }
+
+        [TestMethod]
+        public void EnterChar_NothingEntered_AllLettersNotGuessed()
+        {
+            Word word = new("test");
+            Assert.IsFalse(word.Letters.Where(l => l.HasGuessed).Any());
+        }
+
+        [TestMethod]
+        public void EnterChar_AllEntered_AllLettersGuessed()
+        {
+            Word word = new("test");
+            foreach (Letter letter in word.Letters)
+            {
+                word.EnterChar(letter.Value ?? ' ');
+            }
+            Assert.IsTrue(word.Letters.All(l => l.HasGuessed));
+        }
+    }
+}
