996fad7e454cadd086edc0c05d696f1fa9b3bf2d
BarrackObamanator <144901906+BarrackObamanator@users.noreply.github.com>
2024-11-15 12:55:45 +0100

    Implement data binding in Gamestatestiekscreen with INotifyPropertyChanged
    - Added INotifyPropertyChanged implementation to Gamestatestiekscreen.
    - Exposed properties like AantalWoorden, PercentageFout, etc. for data binding.
    - Set DataContext to 'this' in the constructor to enable binding within the UserControl.
    - Updated the UI to automatically reflect changes in bound properties.

--- TikTovenaar/Gamestatestiekscreen.xaml.cs
+++ TikTovenaar/Gamestatestiekscreen.xaml.cs
@@ -1,38 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TikTovenaar
 {
-    /// <summary>
-    /// Interaction logic for Gamestatestiekscreen.xaml
-    /// </summary>
-    public partial class Gamestatestiekscreen : UserControl
+    public partial class Gamestatestiekscreen : UserControl, INotifyPropertyChanged
     {
+        private string _aantalWoorden;
+        private string _percentageFouten;
+        private string _woordenPerMinuut;
+        private string _totaleTijd;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string AantalWoorden
+        {
+            get => _aantalWoorden;
+            set
+            {
+                _aantalWoorden = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string PercentageFouten
+        {
+            get => _percentageFouten;
+            set
+            {
+                _percentageFouten = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string WoordenPerMinuut
+        {
+            get => _woordenPerMinuut;
+            set
+            {
+                _woordenPerMinuut = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string TotaleTijd
+        {
+            get => _totaleTijd;
+            set
+            {
+                _totaleTijd = value;
+                OnPropertyChanged();
+            }
+        }
+
         public Gamestatestiekscreen()
         {
             InitializeComponent();
+            DataContext = this;
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void Sluit_Click(object sender, RoutedEventArgs e)
         {
-
+            // Add logic to close or hide this screen
+            MessageBox.Show("Scherm gesloten!");
         }
 
         private void Opnieuw_Click(object sender, RoutedEventArgs e)
         {
-
+            // Add logic to reset or restart the game
+            MessageBox.Show("Opnieuw proberen!");
         }
     }
 }
