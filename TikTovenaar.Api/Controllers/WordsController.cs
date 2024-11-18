eea237822ca344a7931e5c09db6cc1218ad3f6c3
KaseToatz1337 <112391293+Raeven69@users.noreply.github.com>
2024-11-18 12:46:36 +0100

    Word caching + pagination

--- TikTovenaar.Api/Controllers/WordsController.cs
+++ TikTovenaar.Api/Controllers/WordsController.cs
@@ -7,20 +7,25 @@ namespace TikTovenaar.Api.Controllers
     [ApiController]
     public class WordsController : ControllerBase
     {
-        [HttpGet]
-        public List<Word> Get()
+        List<Word> Words { get; } = [];
+
+        public WordsController()
         {
-            Database db = new();
-            NpgsqlConnection connection = db.GetConnection();
+            NpgsqlConnection connection = new Database().GetConnection();
             connection.Open();
-            List<Word> words = [];
             using NpgsqlCommand cmd = new(@"SELECT * FROM words", connection);
             using NpgsqlDataReader reader = cmd.ExecuteReader();
             while (reader.Read())
             {
-                words.Add(new(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2)));
+                Words.Add(new(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2)));
             }
-            return words;
+            connection.Close();
+        }
+
+        [HttpGet]
+        public List<Word> Get([FromQuery] int limit = -1, [FromQuery] int minLength = 0, [FromQuery] int maxLength = -1)
+        {
+            return [..Random.Shared.GetItems([..Words.Where((Word word) => word.Length > minLength && (maxLength == -1 || word.Length < maxLength))], (limit == -1) ? Words.Count : limit)];
         }
     }
 }
