50ce004366d9471db41a39beef1316063c8c3d3b
jortk <144902445+jortk@users.noreply.github.com>
2024-11-27 10:47:06 +0100

    Create IDataAccess.cs

---
+++ TikTovenaar.Logic/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TikTovenaar.Logic
+{
+    public interface IDataAccess
+    {
+        public void InsertScore(int userid, int wordsAmount, int timeInSeconds, int score, string[] incorrectWords, string[] incorrectLetters);
+        public Dictionary<string, int> GetScores(); //int is the actual score, the string is the name of the player
+    }
+}
