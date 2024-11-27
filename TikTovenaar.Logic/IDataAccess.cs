a04021cdc70fd6b495b91bb4cd86d80f827d0fb8
jortk <144902445+jortk@users.noreply.github.com>
2024-11-27 11:32:20 +0100

    ...and added all words to the insert parameter

--- TikTovenaar.Logic/IDataAccess.cs
+++ TikTovenaar.Logic/IDataAccess.cs
@@ -8,7 +8,7 @@ namespace TikTovenaar.Logic
 {
     public interface IDataAccess
     {
-        public void InsertScore(int userid, int wordsAmount, int timeInSeconds, int score, string[] incorrectWords, string[] incorrectLetters);
+        public void InsertScore(int userid, int wordsAmount, int timeInSeconds, int score, string[] allWords, string[] incorrectWords, string[] incorrectLetters);
         public Dictionary<string, int> GetScores(); //int is the actual score, the string is the name of the player
     }
 }
