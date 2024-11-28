ef9515917390bb2e010663eb38cb937c27e67777
KäseToatz <112391293+Raeven69@users.noreply.github.com>
2024-11-28 14:54:55 +0100

    Added highscore endpoint

--- TikTovenaar.Api/Controllers/ScoreController.cs
+++ TikTovenaar.Api/Controllers/ScoreController.cs
@@ -8,16 +8,32 @@ namespace TikTovenaar.Api.Controllers
     public class ScoreController : ControllerBase
     {
         [HttpGet]
-        public JsonResult GetScores()
+        public JsonResult GetScore()
         {
+            int? userID = Utils.Authorize(HttpContext.Request);
+            if (userID == null)
+            {
+                return new(new { type = "error", message = "Authorization failed." });
+            }
             NpgsqlConnection connection = new Database().GetConnection();
             connection.Open();
-            using NpgsqlCommand cmd = new(@"SELECT players.name, scores.score FROM players JOIN scores ON players.id = scores.userid", connection);
+            using NpgsqlCommand cmd = new(@"SELECT wordsAmount, time, date, score, incorrectWords, incorrectLetters FROM scores WHERE userid = @userid", connection);
+            cmd.Parameters.AddWithValue("userid", userID);
             using NpgsqlDataReader reader = cmd.ExecuteReader();
-            List<Score> scores = [];
+            List<ScoreEntry> scores = [];
             while (reader.Read())
             {
-                scores.Add(new(reader.GetString(0), reader.GetInt32(1)));
+                ScoreEntry entry = new()
+                {
+                    
+                    WordsAmount = reader.GetInt32(0),
+                    Time = reader.GetFieldValue<TimeOnly>(1),
+                    Date = reader.GetFieldValue<DateOnly>(2),
+                    Value = reader.GetInt32(3),
+                    IncorrectWords = [..reader.GetFieldValue<string[]>(4)],
+                    IncorrectLetters = [..reader.GetFieldValue<char[]>(5)]
+                };
+                scores.Add(entry);
             }
             connection.Close();
             return new(scores);
@@ -43,8 +59,8 @@ namespace TikTovenaar.Api.Controllers
             cmd.Parameters.AddWithValue("time", score.Time);
             cmd.Parameters.AddWithValue("date", score.Date);
             cmd.Parameters.AddWithValue("score", score.Value);
-            cmd.Parameters.AddWithValue("incorrectwords", score.GetIncorrectWords());
-            cmd.Parameters.AddWithValue("incorrectletters", score.GetIncorrectLetters());
+            cmd.Parameters.AddWithValue("incorrectwords", score.IncorrectWords);
+            cmd.Parameters.AddWithValue("incorrectletters", score.IncorrectLetters);
             cmd.ExecuteNonQuery();
             connection.Close();
             return new(new { type = "success", message = "Score added." });
--- TikTovenaar.Api/ScoreEntry.cs
+++ TikTovenaar.Api/ScoreEntry.cs
@@ -6,22 +6,12 @@ namespace TikTovenaar.Api
         public TimeOnly Time { get; set; } = TimeOnly.MinValue;
         public DateOnly Date { get; set; } = DateOnly.MinValue;
         public int Value { get; set; } = -1;
-        public string IncorrectWords { get; set; } = "";
-        public string IncorrectLetters { get; set; } = "";
+        public List<string> IncorrectWords { get; set; } = [];
+        public List<char> IncorrectLetters { get; set; } = [];
 
         public bool IsValid()
         {
             return WordsAmount > -1 && Time != TimeOnly.MinValue && Date != DateOnly.MinValue && Value > -1;
-        }
-
-        public List<string> GetIncorrectWords()
-        {
-            return [..IncorrectWords.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)];
-        }
-
-        public List<char> GetIncorrectLetters()
-        {
-            return [..IncorrectLetters.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(letter => letter[0])];
         }
     }
 }
