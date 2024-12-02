fe5541242dc018ee73764dc28b83cabd76013b5b
GAMERWES01 <148088336+GAMERWES01@users.noreply.github.com>
2024-12-02 12:55:52 +0100

    zoekbalk werkent gemaakt
    de layout is een klein beetje verandert aangezien er nu een zoekbalk is toegevoegt.
    met de pijl knop word er voor gezorgt dat we niet alles in het negatief hoeven te doen maar dat we het met de knop kunnen instellen

--- TikTovenaar/UserStatisticsScreen.xaml.cs
+++ TikTovenaar/UserStatisticsScreen.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,18 +26,9 @@ namespace TikTovenaar
     public partial class UserStatisticsScreen : UserControl
     {
 
-        private int width;
-        public UserStatisticsScreen()
+        StatisticsWord[] woorden =
         {
-            InitializeComponent();
-
-            // zorgt er voor dat alles loaded is zodat ik bijvoorbeeld de breete van het scherm kan lezen
-            this.Loaded += (s, e) =>
-            {
-
-                StatisticsWord[] woorden =
-                {
-                    new StatisticsWord("kindercarnavalsoptochtvoorbereidingswerkzaamheden", 10, 5),
+            new StatisticsWord("kindercarnavalsoptochtvoorbereidingswerkzaamheden", 10, 5),
                     new StatisticsWord("aan", 10, 5),
                     new StatisticsWord("aandoen", 10, 2),
                     new StatisticsWord("aanduiding", 10, 4),
@@ -58,56 +51,28 @@ namespace TikTovenaar
                     new StatisticsWord("aanklacht", 10, 10),
                     new StatisticsWord("aanklacht", 10, 10),
                     new StatisticsWord("aanklacht", 10, 10),
-                };
+        };
 
-                int cellCount = woorden.Length;
-                int numCols = 2;
-                int numRows = (int)Math.Ceiling((double)cellCount / numCols);
+        public ObservableCollection<string> sortOpties { get; set; }
 
-                for (int i = 0; i < numCols; ++i)
-                {
-                    DynamicGrid.ColumnDefinitions.Add(new ColumnDefinition());
-                }
-
-                for (int i = 0; i < numRows; ++i)
-                {
-                    DynamicGrid.RowDefinitions.Add(new RowDefinition());
-                }
-
-
-                foreach (var g in DynamicGrid.RowDefinitions)
-                {
-                    g.Height = new GridLength(ActualHeight/8);
-                }
-
-                foreach (var g in DynamicGrid.ColumnDefinitions)
-                {
-                    g.Width = new GridLength(ActualWidth / numCols);
-                }
-
-                for (int i = 0; i < cellCount; i++)
-                {
-                    Button button = new Button
-                    {
-                        Name = woorden[i].woord,
-                        FontSize = 20,
-                        Content = new TextBlock
-                        {
-                            Text = woorden[i].ToString(),
-                            TextAlignment = TextAlignment.Center
-                        }
-                    };
-
-                    button.Click += Button_Click;
-
-                    DynamicGrid.Children.Add(button);
-
-
-
-                    button.SetValue(Grid.RowProperty, i / numCols);
-                    button.SetValue(Grid.ColumnProperty, i % numCols);
-                }
+        public UserStatisticsScreen()
+        {
+            InitializeComponent();
+            sortOpties = new ObservableCollection<string>
+            {
+                "Alfabetisch",
+                "Aantal goed",
+                "Aantal fout",
+                "Totaal gespeelt"
             };
+
+            // zorgt er voor dat alles loaded is zodat ik bijvoorbeeld de breete van het scherm kan lezen
+            this.Loaded += (s, e) =>
+            {
+                PrintButtons();
+            };
+
+            DataContext = this;
 
         }
 
@@ -124,6 +89,127 @@ namespace TikTovenaar
             MessageBox.Show(clickedButton.Name);
         }
 
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            PrintButtons();
+        }
+
+        private void PrintButtons()
+        {
+            StatisticsWord[] gesorteerdeWoorden;
+
+
+            if (VolgwordenButton.Content.Equals("▲"))
+            {
+                if (SortOptiesButton.SelectedItem.Equals("Alfabetisch"))
+                {
+                    gesorteerdeWoorden = woorden.Where(woord => woord.woord.Contains(searchBar.Text)).OrderBy(w => w.woord).ToArray();
+                }
+                else if (SortOptiesButton.SelectedItem.Equals("Aantal goed"))
+                {
+                    gesorteerdeWoorden = woorden.Where(woord => woord.woord.Contains(searchBar.Text)).OrderBy(w => w.totaalGoed).ToArray();
+                } 
+                else if (SortOptiesButton.SelectedItem.Equals("Aantal fout"))
+                {
+                    gesorteerdeWoorden = woorden.Where(woord => woord.woord.Contains(searchBar.Text)).OrderBy(w => w.totaalFout).ToArray();
+                }
+                else //if (SortOptiesButton.SelectedItem.Equals("Totaal gespeelt"))
+                {
+                    gesorteerdeWoorden = woorden.Where(woord => woord.woord.Contains(searchBar.Text)).OrderBy(w => w.totaalGespeelt).ToArray();
+                }
+            }
+            else
+            {
+                if (SortOptiesButton.SelectedItem.Equals("Alfabetisch"))
+                {
+                    gesorteerdeWoorden = woorden.Where(woord => woord.woord.Contains(searchBar.Text)).OrderByDescending(w => w.woord).ToArray();
+                }
+                else if (SortOptiesButton.SelectedItem.Equals("Aantal goed"))
+                {
+                    gesorteerdeWoorden = woorden.Where(woord => woord.woord.Contains(searchBar.Text)).OrderByDescending(w => w.totaalGoed).ToArray();
+                }
+                else if (SortOptiesButton.SelectedItem.Equals("Aantal fout"))
+                {
+                    gesorteerdeWoorden = woorden.Where(woord => woord.woord.Contains(searchBar.Text)).OrderByDescending(w => w.totaalFout).ToArray();
+                }
+                else //if (SortOptiesButton.SelectedItem.Equals("Totaal gespeelt"))
+                {
+                    gesorteerdeWoorden = woorden.Where(woord => woord.woord.Contains(searchBar.Text)).OrderByDescending(w => w.totaalGespeelt).ToArray();
+                }
+                
+            }
+
+            
+
+            DynamicGrid.Children.Clear();
+            DynamicGrid.RowDefinitions.Clear();
+            DynamicGrid.ColumnDefinitions.Clear();
+            int cellCount = gesorteerdeWoorden.Length;
+            int numCols = 2;
+            int numRows = (int)Math.Ceiling((double)cellCount / numCols);
+
+            for (int i = 0; i < numCols; ++i)
+            {
+                DynamicGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < numRows; ++i)
+            {
+                DynamicGrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+
+            foreach (var g in DynamicGrid.RowDefinitions)
+            {
+                g.Height = new GridLength(ActualHeight / 8);
+            }
+
+            foreach (var g in DynamicGrid.ColumnDefinitions)
+            {
+                g.Width = new GridLength(ActualWidth / numCols);
+            }
+
+            for (int i = 0; i < cellCount; i++)
+            {
+                Button button = new Button
+                {
+                    Name = gesorteerdeWoorden[i].woord,
+                    FontSize = 20,
+                    Content = new TextBlock
+                    {
+                        Text = gesorteerdeWoorden[i].ToString(),
+                        TextAlignment = TextAlignment.Center
+                    }
+                };
+
+                button.Click += Button_Click;
+
+                DynamicGrid.Children.Add(button);
+
+
+
+                button.SetValue(Grid.RowProperty, i / numCols);
+                button.SetValue(Grid.ColumnProperty, i % numCols);
+            }
+        }
+
+        private void VolgwordenButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (VolgwordenButton.Content.Equals("▲"))
+            {
+                VolgwordenButton.Content = "▼";
+            }
+            else
+            {
+                VolgwordenButton.Content = "▲";
+            }
+            PrintButtons();
+        }
+
+        private void SortOptiesVerandering(object sender, SelectionChangedEventArgs e)
+        {
+            PrintButtons();
+        }
     }
 }
  
