7f66e3f362930d24ba82844e0fb386229d67e35a
KäseToatz <112391293+Raeven69@users.noreply.github.com>
2024-12-06 14:17:51 +0100

    Add login state singleton

---
+++ TikTovenaar.Logic/CurrentUser.cs
@@ -0,0 +1,23 @@
+﻿namespace TikTovenaar.Logic
+{
+    public class CurrentUser
+    {
+        private static readonly CurrentUser instance = new();
+        private CurrentUser() {}
+        public static CurrentUser Instance => instance;
+
+        public bool IsSet { get; private set; } = false;
+        public string Name { get; private set; } = "";
+        public string Token { get; private set; } = "";
+
+        public void Set(string name, string token)
+        {
+            if (!IsSet)
+            {
+                Name = name;
+                Token = token;
+                IsSet = true;
+            }
+        }
+    }
+}
