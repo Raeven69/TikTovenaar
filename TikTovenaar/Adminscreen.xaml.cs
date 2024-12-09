d414ce151995233f55edc7fb28e3e19a520a513b
jortk <s1200713@student.windesheim.nl>
2024-12-09 13:57:18 +0100

    Added animations and changed buttons

--- TikTovenaar/Adminscreen.xaml.cs
+++ TikTovenaar/Adminscreen.xaml.cs
@@ -20,12 +20,18 @@ namespace TikTovenaar
     /// </summary>
     public partial class AdminScreen : UserControl
     {
+        private WizardAnimation _wizardAnimation1;
+        private WizardAnimation _wizardAnimation2;
+
         public AdminScreen()
         {
             InitializeComponent();
+            _wizardAnimation1 = new(wizardIdleImageBrush, 0.16666, 6);
+            _wizardAnimation1.StartAnimation(0.16666, 6, "Images/wizard_idle.png");
+            _wizardAnimation2 = new(wizardJumpImageBrush, 0.16666, 6);
+            _wizardAnimation2.StartAnimation(0.16666, 6, "Images/wizard_idle.png");
         }
 
-        
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
