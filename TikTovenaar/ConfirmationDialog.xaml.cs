7f69a6cc6a009b827cf417d93c68099118dc9055
BarrackObamanator <s1197014@student.windesheim.nl>
2024-12-17 10:31:25 +0100

    sup bor

---
+++ TikTovenaar/ConfirmationDialog.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TikTovenaar.DataAccess;
+using TikTovenaar.Logic;
+
+namespace TikTovenaar
+{
+    public partial class ConfirmDialog : Window
+    {
+        private String _name;
+        public ConfirmDialog(String name)
+        {
+            this._name = name;
+            InitializeComponent();
+        }
+
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataHandler dataHandler = new DataHandler();
+            try
+            {
+                dataHandler.DeleteUser(CurrentUser.Instance.Token, _name);
+                this.Close();
+            }
+            catch (RequestFailedException ex)
+            {
+                subTitleText.Text = $"Kan {_name} niet verwijderen";
+                subTitleText.Foreground = Brushes.Red;
+            }
+        }
+
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
